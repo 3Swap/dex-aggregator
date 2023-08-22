f3f39ce233be8f33d618a7114a545de583fc713d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-22 21:02:31 +0100

    update; set up fotns

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito_Sans } from "next/font/google"
+
+const nunito_sans = Nunito_Sans({
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nunito_sans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
