7c029f5b9bb6d2b52c68e1521aa8461be7d3acba
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-11 21:36:32 +0100

    update: change icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/icon.svg" />
+        <link rel="icon" href="/3swap.svg" />
       </Head>
       <body>
         <Main />
