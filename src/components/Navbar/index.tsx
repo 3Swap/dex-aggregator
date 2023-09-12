bed1766abba8c9fb0e8d1f43695332a0907e910c
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-12 09:00:42 +0100

    update: add caret and link

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image"
 import Link from "next/link"
+import { PiCaretRightBold } from 'react-icons/pi'
 
 export default function Navbar({ }) {
     return (
         <div className="w-full flex justify-between items-center pt-[1rem] px-5 z-10">
-            <div>
+            <Link href="/">
                 <Image src="/3swap.svg" width={796} height={208} alt="3swap" className="w-auto h-10" />
-            </div>
+            </Link>
             <div className="flex items-center gap-3">
                 <div className="items-center gap-3 hidden sm:flex">
                     <p className="text-white font-bold text-xl">EN</p>
                     <Image src="/bg/world.svg" width={30} height={30} alt="world icon" />
                 </div>
-                <Link href="/" className="px-6 py-2 bg-[#4500A0] text-white rounded-lg shadow-md">
-                    Launch dApp {'>'}
+                <Link href="/swap" className="px-6 py-2 bg-[#4500A0] text-white rounded-lg shadow-md inline-flex items-center">
+                    Launch dApp <PiCaretRightBold />
                 </Link>
             </div>
         </div>
