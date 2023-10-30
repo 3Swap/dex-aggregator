a7ed3b709504d17250b95e0a23d7f128f9e81eea
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-10-30 08:18:45 +0100

    update: set up link visible

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,13 +2,13 @@ import Image from "next/image"
 import Link from "next/link"
 import { PiCaretRightBold } from 'react-icons/pi'
 
-export default function Navbar({ }) {
+export default function Navbar({ visibleLink }: any) {
     return (
         <div className="w-full flex justify-between items-center pt-[1rem] px-5 z-10">
             <Link href="/">
                 <Image src="/3swap.svg" width={796} height={208} alt="3swap" className="w-auto h-10" />
             </Link>
-            <div className="flex items-center gap-3">
+            <div className={`items-center gap-3 ${visibleLink ? "flex" : "hidden"}`}>
                 <div className="items-center gap-3 hidden sm:flex">
                     <p className="text-white font-bold text-xl">EN</p>
                     <Image src="/bg/world.svg" width={30} height={30} alt="world icon" />
