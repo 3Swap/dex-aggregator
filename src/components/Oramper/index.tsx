52ac856d7241f44fdf1e9629846f01f16e8505b7
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-12-07 10:43:38 +0100

    fix: loader

--- src/components/Oramper/index.tsx
+++ src/components/Oramper/index.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
+import { LoadingIndicator } from '@/components/Loading'
 
 export const Oramper = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    function handleIframeLoad() {
+        setIsLoading(false)
+    }
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -8,8 +17,11 @@ export const Oramper = () => {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.2 }}
         >
+            {isLoading && <LoadingIndicator />}
             <iframe src="https://onramp.money/main/sell/?appId=761602"
-            className='rounded-lg h-screen w-[375px] shadow-md'
+                className='rounded-lg h-screen w-[375px] shadow-md'
+                onLoad={handleIframeLoad}
+                style={{ display: isLoading ? 'none' : 'block' }}
             ></iframe>
         </motion.div>
     )
