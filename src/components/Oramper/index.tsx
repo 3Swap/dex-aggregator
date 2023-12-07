c304675fc0222441b2e4db643a08cb8b2b617aec
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-12-07 10:35:06 +0100

    fix: replaced integration

--- src/components/Oramper/index.tsx
+++ src/components/Oramper/index.tsx
@@ -1,31 +1,4 @@
-import { useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { OnrampWebSDK } from '@onramp.money/onramp-web-sdk';
-import { LoadingIndicator } from '@/components/Loading';
-
-const OnrampWidget = () => {
-    useEffect(() => {
-        // Initialize Onramp SDK
-        const onrampInstance = new OnrampWebSDK({
-            appId: 761602, 
-            coinCode: 'usdt',
-            network: 'erc20',
-            fiatAmount: 1000,
-            flowType: 2,
-            walletAddress: '0xF62bf9E069B08a6e479208A36aDFE668701BCD94'
-        });
-
-        // Show the widget when ready
-        onrampInstance.show();
-
-        // Clean up the widget when the component is unmounted
-        return () => {
-            onrampInstance.close();
-        };
-    }, []); 
-
-    return <LoadingIndicator />;
-};
 
 export const Oramper = () => {
     return (
@@ -35,7 +8,9 @@ export const Oramper = () => {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.2 }}
         >
-            <OnrampWidget />
+            <iframe src="https://onramp.money/main/sell/?appId=761602"
+            className='rounded-lg h-screen w-[375px] shadow-md'
+            ></iframe>
         </motion.div>
     )
 }
